b460459586bb185c9fd8484e7ccb324c13609df4
DevfoxJeongHoeJun <jeonghj@devfox.co.jp>
2025-08-14 16:42:46 +0900

    2025/08/14 GroupController update

--- src/main/kotlin/com/attend/attendance_api/controller/GroupController.kt
+++ src/main/kotlin/com/attend/attendance_api/controller/GroupController.kt
@@ -3,10 +3,13 @@ package com.attend.attendance_api.controller
 import com.attend.attendance_api.common.ApiResponse
 import com.attend.attendance_api.dto.AttendListRequest
 import com.attend.attendance_api.dto.AttendListResponse
+import com.attend.attendance_api.dto.LoginResDto
 import com.attend.attendance_api.service.GroupService
 import com.attend.attendance_api.service.UserService
+import jakarta.servlet.http.HttpSession
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -15,13 +18,19 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/group")
 class GroupController(
     private val groupService: GroupService,
-    private val userService: UserService
+    private val userService: UserService,
 ) {
 
-    @GetMapping("/attend-list")
+    @PostMapping("/attend-list")
     fun getAttendList(
-        @RequestBody request: AttendListRequest
+        @RequestBody request: AttendListRequest,
+        session: HttpSession
     ): ApiResponse<List<AttendListResponse>>{
+        // Session ユーザーチェック
+        if (session.getAttribute("userId") == null) {
+            return ApiResponse(HttpStatus.UNAUTHORIZED, "失敗",null);
+        }
+
         val response:List<AttendListResponse> = groupService.getAttendList(request)
 
         return ApiResponse(HttpStatus.OK, "成功", response)
