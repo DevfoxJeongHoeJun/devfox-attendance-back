4db83772e1fd07f879e686a97ba8180d5a39f349
ハンサンヒョン <hansh@devfox.co.jp>
2025-08-18 17:30:33 +0900

    backtoorigin

--- src/main/kotlin/com/attend/attendance_api/controller/AppController.kt
+++ src/main/kotlin/com/attend/attendance_api/controller/AppController.kt
@@ -1,14 +1,36 @@
 package com.attend.attendance_api.controller
 
+import com.attend.attendance_api.dto.UserRequest
+import com.attend.attendance_api.service.AppService
 import com.attend.attendance_api.service.GroupService
-import com.attend.attendance_api.service.UserService
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.http.ResponseEntity
+import com.attend.attendance_api.dto.UserResponse
 
+@CrossOrigin(origins = ["http://localhost:60259"])
 @RestController
 @RequestMapping("/api/app")
 class AppController(
     private val groupService: GroupService,
-    private val userService: UserService
+    private val appService: AppService
 ) {
+
+    @GetMapping("/{id}")
+    fun getUser(@PathVariable id: Long): ResponseEntity<UserResponse> {
+        val user = appService.getUserById(id)
+        return ResponseEntity.ok(user)
+    }
+
+    @PostMapping("/update")
+    fun updateUser(@RequestBody request: UserRequest): ResponseEntity<String> {
+        appService.updateUser(request)
+        return ResponseEntity.ok("更新完了")
+    }
+
+    @DeleteMapping("/delete/{id}")
+    fun deleteUser(@PathVariable id: Long): ResponseEntity<String> {
+        appService.deleteUser(id)
+        return ResponseEntity.ok("削除完了")
+    }
+
 }
--- src/main/kotlin/com/attend/attendance_api/service/AppService.kt
+++ src/main/kotlin/com/attend/attendance_api/service/AppService.kt
@@ -1,12 +1,36 @@
 package com.attend.attendance_api.service
 
+import com.attend.attendance_api.dto.UserRequest
 import com.attend.attendance_api.repository.GroupRepository
 import com.attend.attendance_api.repository.UserRepository
 import org.springframework.stereotype.Service
+import com.attend.attendance_api.dto.UserResponse
 
 @Service
 class AppService(
     private val userRepository: UserRepository,
     private val groupRepository: GroupRepository
 ) {
+    fun getUserById(id: Long): UserResponse {
+        val user = userRepository.findById(id).orElseThrow()
+        return UserResponse(
+            id = user.id!!,
+            name = user.name,
+            email = user.email
+        )
+    }
+
+    fun updateUser(request: UserRequest) {
+        val user = userRepository.findById(request.id).orElseThrow()
+        user.name = request.name
+        user.email = request.email
+        user.password = request.password
+        userRepository.save(user)
+    }
+
+    fun deleteUser(id: Long) {
+        userRepository.deleteById(id)
+    }
+
+
 }
