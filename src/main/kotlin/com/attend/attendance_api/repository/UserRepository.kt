2f9eb232380329104bc8c08a21c3b98b5dc1dd6c
kimsunhyeon-devfox <kimsunhyeon@devfox.co.jp>
2025-08-18 14:54:47 +0900

    UserRepository

--- src/main/kotlin/com/attend/attendance_api/repository/UserRepository.kt
+++ src/main/kotlin/com/attend/attendance_api/repository/UserRepository.kt
@@ -1,7 +1,10 @@
 package com.attend.attendance_api.repository
 
 import com.attend.attendance_api.dto.AttendListResponse
+import com.attend.attendance_api.dto.UsersListResponse
 import com.attend.attendance_api.entity.UserEntity
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
@@ -11,4 +14,59 @@ import java.util.Optional
 interface UserRepository: JpaRepository<UserEntity, Long> {
     fun findByEmail(email: String): Optional<UserEntity>
 
+    @Query(
+        value = """
+        SELECT 
+            u.id AS user_id,
+            u.name AS user_name,
+            g.name AS group_name,
+            u.access_level_code
+        FROM users u
+        LEFT JOIN groups g
+            ON u.group_code = g.code
+        WHERE u.group_code = :groupCode
+          AND (u.access_level_code = 'ROLE_USER' OR u.access_level_code = 'ROLE_MANAGER')
+          AND u.name LIKE CONCAT('%', :userName, '%')
+        ORDER BY u.id
+    """,
+        countQuery = """
+        SELECT COUNT(*)
+        FROM users u
+        LEFT JOIN groups g
+            ON u.group_code = g.code
+        WHERE u.group_code = :groupCode
+          AND (u.access_level_code = 'ROLE_USER' OR u.access_level_code = 'ROLE_MANAGER')
+          AND u.name LIKE CONCAT('%', :userName, '%')
+    """,
+        nativeQuery = true
+    )
+    fun getUsersWithGroupSearch(
+        @Param("groupCode") groupCode: String,
+        @Param("userName") userName: String,
+        pageable: Pageable
+    ): Page<AttendListResponse>
+
+    @Query(
+        value = """
+    SELECT 
+        u.id AS user_id,
+        u.name AS user_name,
+        g.name AS group_name,
+        u.access_level_code
+    FROM users u
+    LEFT JOIN groups g
+        ON u.group_code = g.code
+    WHERE (u.access_level_code = 'ROLE_USER' OR u.access_level_code = 'ROLE_MANAGER')
+    ORDER BY u.id
+""",
+        countQuery = """
+    SELECT COUNT(*)
+    FROM users u
+    LEFT JOIN groups g
+        ON u.group_code = g.code
+    WHERE (u.access_level_code = 'ROLE_USER' OR u.access_level_code = 'ROLE_MANAGER')
+""",
+        nativeQuery = true
+    )
+    fun getUsersWithGroup(pageable: Pageable): Page<UsersListResponse>
 }
